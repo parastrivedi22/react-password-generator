4c4fab3f37baaf32be6b428c57ab3fe3c91bf10e
Paras Trivedi <parastrivedi22@gmail.com>
2023-12-30 14:07:15 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   
+    <div>
+      {/* display  flied */}
+      <div>
+        <input type="text" readOnly value={"kjdfklad"} />
+        
+        <button
+        type="button"
+        className="rounded-md bg-blue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-black"
+      >copy</button>
+      </div>
+
+      {/* button sestion */}
+    </div>
+
+
+   </>
+  )
+}
+
+export default App
