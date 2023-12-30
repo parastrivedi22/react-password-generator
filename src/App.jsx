84bf7401b61319c972c230f12633a69b45d8695d
Paras Trivedi <parastrivedi22@gmail.com>
2023-12-30 23:28:50 +0530

    all hooks set

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,66 @@
+import { useState, useEffect, useCallback } from 'react'
 import './App.css'
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [allowedNumber, setAllowedNumber] = useState(true);
+  const [allowedChar, setAllowedChar] = useState(false);
+  const [password, setPassword] = useState("adklfjdskj");
+
+
+  const passwordGenerator = useCallback(()=>{
+    let pwd ="";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(allowedChar) str+="@#$%&";
+    if(allowedNumber) str+="1234567890";
+
+    for(let i=1; i<=length; i++){
+      pwd +=str.charAt(Math.floor(Math.random()*str.length+1));
+    }
+
+    setPassword(pwd);
+  }, [length, allowedChar, allowedNumber, setPassword])
+
+useEffect(()=>{
+  passwordGenerator();
+}, 
+[length,allowedChar,allowedNumber, passwordGenerator])
 
   return (
    <>
    
-    <div>
+    <div className='  bg-white  shadow-md p-20 rounded-lg w-1/2 m-auto'>
       {/* display  flied */}
-      <div>
-        <input type="text" readOnly value={"kjdfklad"} />
+      <div style={{boxShadow:"0 0 10px 1px black"}} className='rounded-lg flex items-stretch'>
+        <input className='size-full px-3 py-2  rounded-s-lg  outline-none' type="text" readOnly value={password} />
         
         <button
         type="button"
-        className="rounded-md bg-blue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-2 focus-visible:outline-black"
-      >copy</button>
+        className="  rounded-e-md bg-blue-600 px-3 py-2  font-semibold text-white hover:bg-blue-600/80"
+      >
+        Copy
+      </button>
       </div>
 
       {/* button sestion */}
+       
+
+      <div className='mt-10 flex justify-between' >
+        
+      <span>
+      <input type="range" min={8} max={16} value={length} id='range' onChange={(event)=>setLength(event.target.value)}  />
+     <label htmlFor="range"> Length {length}</label>
+      </span>
+      <span >
+     <input type="checkbox" id='numberAllowed' defaultChecked={allowedNumber} onChange={()=>{setAllowedNumber((prev)=>!prev)}} />
+     <label htmlFor="numberAllowed">Number</label>
+     </span>
+     <span>
+     <input type="checkbox" id='charAllowed'  onChange={()=>{setAllowedChar((prev)=>!prev)}} />
+     <label htmlFor="charAllowed">Character</label></span>
+      </div>
+
+
     </div>
 
 
